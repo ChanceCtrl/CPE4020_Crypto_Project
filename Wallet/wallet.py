82f6005018c07e688bbe82fb76906a1cbbde8f55
ChanceCtrl <142356358+ChanceCtrl@users.noreply.github.com>
2026-04-20 11:22:20 -0400

    mrowb

--- Wallet/wallet.py
+++ Wallet/wallet.py
@@ -1,10 +1,19 @@
+from json import dumps
+
 import rsa
+from rsa import PublicKey
 
 
 class evWalletChain:
     known_wallets: list[str]
 
-    def register_new_wallet(self, public_key: str, signature: str) -> bool:
+    def register_new_wallet(
+        self, data: dict[str, str | int | float], signature: str
+    ) -> bool:
+        # Check if they own that public_key
+        self.auth_wallet(data, signature)
+
+        public_key = str(data.get("walletKey"))
 
         # Check if any wallets already have that public_key stored
         if public_key in self.known_wallets:
@@ -14,5 +23,17 @@ class evWalletChain:
 
         return True
 
-    def auth_wallet(self, data, signature: str) -> bool:
+    def auth_wallet(self, data: dict[str, str | int | float], signature: str) -> bool:
+        public_key = data.get("walletKey")
+
+        try:
+            rsa.verify(dumps(data), signature.encode(), PublicKey(public_key))
+        except Exception as E:
+            print(f"Error verifing: {E}")
+            return False
         return True
+
+
+# fella = evWalletChain()
+#
+# fella.register_new_wallet()
